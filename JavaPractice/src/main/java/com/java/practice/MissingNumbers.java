79c27f353566ff88bc332c8ac863114d3989fb9b
Rajat Shandilya <rajatshandilya25@gmail.com>
2020-09-08 15:40:02 +0530

    added new program: Divide string in equal parts

--- JavaPractice/src/main/java/com/java/practice/MissingNumbers.java
+++ JavaPractice/src/main/java/com/java/practice/MissingNumbers.java
@@ -9,7 +9,7 @@ public class MissingNumbers {
 		
 		 int array[] = {1, 2, 4, 5, 6, 10, 0};
 		 int n = 0;
-		  Arrays.sort(array);
+		  Arrays.sort(array);  //0,1,2,4,5,6,10
 		   
 		  for(int i=0; i< array[array.length -1]; i++)
 		  {
